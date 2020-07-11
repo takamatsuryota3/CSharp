57a845285a9fd389f76559d567864991605c1741
takamatsuryota3 <tj.ryota3@gmail.com>
2020-07-11 13:06:00 +0900

    RawImageViewを作成

---
+++ RawImageView/RawImageView/MainWIndowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MVVMBase;
+
+namespace RawImageView
+{
+    class MainWIndowViewModel:ViewModelBase
+    {
+
+        #region 
+        private DelegateCommand _openCommnad;
+        public DelegateCommand OpenCommand
+        {
+            get
+            {
+                if (_openCommnad == null)
+                {
+                    _openCommnad = new DelegateCommand(
+                        ()=> { OnOpenCommand(); });
+                }
+                return _openCommnad;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public MainWIndowViewModel(MainWindow main)
+        {
+
+        }
+        #endregion
+
+        #region Private Method
+        private void OnOpenCommand()
+        {
+            MessageBox.Show("Hello World!");
+        }
+        #endregion
+    }
+}
